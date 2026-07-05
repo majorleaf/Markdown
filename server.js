75ac206892df1fa0a93f67aa028c5f0d20eb0553
Madandola james <jamesmadandola@gmail.com>
2026-07-05 06:04:35 +0100

    fs

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
+// Built-in Node_module for interacting with file system 
 import fs from 'fs';
+//Built-in Node module for cryptography and random IDs
 import crypto from 'crypto';
 
 
@@ -9,8 +11,16 @@ dotenv.config();
 const PORT = process.env.PORT || 8000;
 const app = express();
 
-
+// middleware to parse incoming JSON bodies
 app.use(express.json());
+
+
+// automaticatically create "notes" folder if it doesn't exists 
+const notesDir = './notes';
+if (!fs.existsSync(notesDir)) {
+    fs.mkdirSync(notesDir);
+    console.log(`Created directory: ${notesDir}`);
+}
 
  
 app.get ('/', (req, res) =>  {
@@ -18,7 +28,37 @@ app.get ('/', (req, res) =>  {
     res.send("Markdown API is running");
 }
 );
+ 
+app.post('/note', (req, res) => {
+    try { 
+        // take the note from the incoming request body
+        const { note } = req.body;
+        // check if the user actually sent some text 
+        if(!note) {
+            return res.status(400).json({ error: "provide 'note' text in JSON body "});
+        }
 
+        // generate a file name using crypto 
+        const filename = `${crypto.randomUUID()}.md`;
+
+        // //   To define the fie path where the file will be saved 
+        const filepath = `${notesDir}/${filename}`;
+
+        // write the file to the file system
+        fs.writeFileSync(filepath, note);
+
+        req.status(500).json({
+            message: "Note saved successfully",
+            filename: filename
+        });
+
+
+    } catch (error) {
+        console.error("Error saving note", error);
+        res.status(500).json({ error: " failed to save note"});
+
+    }
+})
 
 
 app.listen(8000, () => { 
